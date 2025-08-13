7acc1797f87cd53fc765df57ff088913e2467035
ItsVikasA <vikas.ambalazari@gmail.com>
2025-08-13 20:09:17 +0530

    Add dotenv support and .env file for environment variables

---
+++ Apna College/MERN Stack/43.MangoDB With Express/env-setup.js
@@ -0,0 +1,4 @@
+// Load environment variables from .env if available
+require('dotenv').config();
+
+// ...existing code...
--- Apna College/MERN Stack/43.MangoDB With Express/init.js
+++ Apna College/MERN Stack/43.MangoDB With Express/init.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 const Chat = require("./models/chat.js");
 
